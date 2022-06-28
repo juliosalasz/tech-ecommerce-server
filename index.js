5d9306e9733bca5e1bb8746d89b205aebdc5ad6f
Julio Salas <jscomic@gmail.com>
2022-06-27 20:20:22 -0500

    change to port

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ const PORT = process.env.PORT || 5000;
 mongoose
   .connect(CONNECTION_URL)
   .then(() =>
-    app.listen(PORT, () => {
+    app.listen(process.env.PORT || 5000, () => {
       console.log(`Server has been started on ${PORT}`);
     })
   )
